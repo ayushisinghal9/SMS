ebdcfd4d974e90271b86ac5ffc7f4ad0bdbb368a
ayushisinghal9 <50760849+ayushisinghal9@users.noreply.github.com>
2021-10-05 17:57:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var path        = require('path');
 var request     = require('request');
 var routes      = require('./routes');
 var activity    = require('./routes/activity');
+console.log("activity"+activity);
 const SFClient = require('./utils/sfmc-client');
 var app = express();
 
